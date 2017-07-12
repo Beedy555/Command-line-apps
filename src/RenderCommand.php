c92d9313153e22a54e0587deaff55e3aada7c08a
Lamberts <matt@Lambertss-MacBook-Air.local>
2017-07-12 22:30:42 +0100

    render table data

---
+++ src/RenderCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Acme;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RenderCommand extends Command
+{
+
+    public function configure()
+    {
+        $this->setName('render')
+            ->setDescription('Render some tabular data');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Age'])
+            ->setRows([
+                ['John Doe', 30],
+                ['Matt Smith', 28],
+                ['Claire Hill', 31]
+            ])
+            ->render();
+    }
+}
